b13d16dcdfa562892be18b79054faf5017a4370d
kervines <kervinzero@gmail.com>
2023-09-16 23:45:47 -0300

    implementação animar ao scroll

--- script.js
+++ script.js
@@ -62,8 +62,31 @@ function initScrollSuave() {
     behavior: 'smooth',
   });*/
   }
-
   linksInternos.forEach((link) => {
     link.addEventListener('click', scrollToSection);
   });
 }
+initScrollSuave();
+
+//Animação ao Scroll
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.5;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimacaoScroll();
